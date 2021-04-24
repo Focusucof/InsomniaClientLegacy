849082e568180bc1ffe6a4d6c5d7a1a29005a266
ImTheFocus <65309793+ImTheFocus@users.noreply.github.com>
2021-04-24 17:20:39 -0400

    -1.00

--- src/event.js
+++ src/event.js
@@ -2,9 +2,21 @@ async function createParty() {
     var response = await fetch("http://127.0.0.1:1337/party/v1/create", {
         method: "GET"
     });
-    return response
+    return response;
+    
+}
+
+async function joinParty() {
+    var response = await fetch(`http://127.0.0.1:1337/party/v1/join/${document.getElementById("partyid").value}`, {
+            method: "GET"
+    });
+    return response;
 }
 
 document.querySelector("#createlobby").addEventListener("click", async () => {
     await createParty();
+});
+
+document.querySelector("#joinlobby").addEventListener("click", async () => {
+    await joinParty();
 });
